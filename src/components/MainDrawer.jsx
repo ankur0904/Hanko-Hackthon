5bb51e13d95ef3e00816163585fb878d1915e8b1
ankur0904 <ankursingh91002@gmail.com>
2023-10-21 23:55:40 +0530

    Completed MainDrawer

--- src/components/MainDrawer.jsx
+++ src/components/MainDrawer.jsx
@@ -1,65 +1,63 @@
 import React from 'react';
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Text,
-    Button,
-    Stack,
-    Avatar,
-    useDisclosure,
-    Input
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Text,
+  Button,
+  Stack,
+  Avatar,
+  useDisclosure,
+  Flex
 } from '@chakra-ui/react';
 
 function MainDrawer() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = React.useRef()
-  
-    return (
-      <>
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <>
+      <Flex justifyContent="center">
         <Button ref={btnRef} colorScheme='teal' onClick={onOpen} m='3'>
           Drawer
         </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='left'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Your files are here :)</DrawerHeader>
-  
-            <DrawerBody>
+      </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Your files are here :)</DrawerHeader>
+
+          <DrawerBody>
             <Stack spacing={1} align="center">
-                    <Avatar bg='teal.500' size='xl' mt={16} />
-                    <Text fontSize='xl' mt={4}>Welcome, Ankur</Text>
-                </Stack>
-                <Stack mt={12}>
-                    <Button ml={4} mr={4} mt={1} >Your Photos</Button>
-                    <Button ml={4} mr={4} mt={1} >Your PDF documents</Button>
-                    <Button ml={4} mr={4} mt={1} >Your Important Files</Button>
-                    {/* <Button ml={4} mr={4} mt={1} ></Button> */}
-                </Stack>
-            </DrawerBody>
-  
-            <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='blue'>Save</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
-  }
+              <Avatar bg='teal.500' size='xl' mt={16} />
+              <Text fontSize='xl' mt={4}>Welcome,</Text>
+            </Stack>
+            <Stack mt={12}>
+              <Button as={'a'} ml={4} mr={4} mt={1} href={'list'}>Your Photos</Button>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button as={'a'} colorScheme='blue' href={'contactus'}>Need Help</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
 
 export default MainDrawer;
 
-               
